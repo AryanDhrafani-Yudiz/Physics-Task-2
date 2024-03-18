4444aba83086a7c374a3d275bba261ff6a940074
AryanDhrafani-Yudiz <aryan.dhrafani@yudiz.com>
2024-03-18 19:03:45 +0530

    Update PlayerMovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -19,6 +19,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Rigidbody2D rbOfGrapplePlatform;
     [SerializeField] private float distanceChange;
     [SerializeField] Vector2 velocityChange;
+    private bool canMove = true;
 
     private void Awake()
     {
@@ -64,39 +65,47 @@ public class PlayerMovement : MonoBehaviour
     }
     private void SpidermanMovement() // Adds Force To Player To Demonstrate Jumping Like Movement
     {
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (canMove)
         {
-            playerSpringJoint2D.distance += distanceChange;
-        }
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            if (playerSpringJoint2D.distance > 1.5f)
+            if (Input.GetKey(KeyCode.UpArrow))
             {
-                playerSpringJoint2D.distance -= distanceChange;
+                playerSpringJoint2D.distance += distanceChange;
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                if (playerSpringJoint2D.distance > 1.5f)
+                {
+                    playerSpringJoint2D.distance -= distanceChange;
+                }
+            }
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                playerRigidBody.velocity -= velocityChange;
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                playerRigidBody.velocity += velocityChange;
+            }
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                playerSpringJoint2D.connectedBody = null;
+                playerSpringJoint2D.enabled = false;
+                canMove = false;
             }
         }
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (canMove == false)
         {
-            playerRigidBody.velocity -= velocityChange;
-        }
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            playerRigidBody.velocity += velocityChange;
-        }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            playerSpringJoint2D.connectedBody = null;
-            playerSpringJoint2D.enabled = false;
-        }
-        if (Input.GetMouseButtonDown(0))
-        {
-            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, 0f, 1 << 3);
-
-            if (hit.collider != null)
+            if (Input.GetMouseButtonDown(0))
             {
-                playerSpringJoint2D.enabled = true;
-                rbOfGrapplePlatform = hit.collider.gameObject.GetComponent<Rigidbody2D>();
-                playerSpringJoint2D.connectedBody = rbOfGrapplePlatform;
+                RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero, 0f, 1 << 3);
+
+                if (hit.collider != null)
+                {
+                    playerSpringJoint2D.enabled = true;
+                    rbOfGrapplePlatform = hit.collider.gameObject.GetComponent<Rigidbody2D>();
+                    playerSpringJoint2D.connectedBody = rbOfGrapplePlatform;
+                    canMove = true;
+                }
             }
         }
     }
